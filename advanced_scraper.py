12d6d021c3505d884883e887acbf9b0a4aad5298
shakibahmed35 <shakibahmed35@gmail.com>
2021-11-02 23:34:44 -0700

    im gonna kill myself

--- advanced_scraper.py
+++ advanced_scraper.py
@@ -3,34 +3,36 @@ from bs4 import BeautifulSoup
 import csv
 
 
-year = 1980
+year = 2021
 while year <= 2021:
     type_of_stat = "advanced"
     URL = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_" + type_of_stat + ".html"
     filename = "./" + type_of_stat + "_stats/" + str(year) + type_of_stat + "_stats.csv"
 
-    f = open(filename, 'w')
+    # f = open(filename, 'w')
 
-    writer = csv.writer(f)
-
+    # writer = csv.writer(f)
+    f = open("text.txt", "w")
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id="all_advanced_stats")
-    players = results.find_all("tr", class_="full_table")
+    f.write(str(soup))
+    results = soup.find(id="csv_per_game_stats")
+    print(results)
+    # players = results.find_all("tr", class_="full_table")
 
-    # Generating Headers
-    headers = []
-    for element in players[0].find_all():
-        headers.append(element.get("data-stat"))
-    writer.writerow(headers)
+    # # Generating Headers
+    # headers = []
+    # for element in players[0].find_all():
+    #     headers.append(element.get("data-stat"))
+    # # writer.writerow(headers)
 
-
-    for player in players:
-        all_stats = player.find_all()
-        stats = []
-        for element in all_stats:
-            stats.append(element.text)
-        writer.writerow(stats)
+    # print(results.prettify())
+    # for player in players:
+    #     all_stats = player.find_all()
+    #     stats = []
+    #     for element in all_stats:
+    #         stats.append(element.text)
+    #     # writer.writerow(stats)
 
     f.close()
     year += 1
---
+++ csv_scraper.py
@@ -0,0 +1,60 @@
+import sys
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+
+driver_path = "chromedriver_win32/chromedriver.exe"
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(executable_path=driver_path, options=options)
+
+
+
+def getcsv(current_url, lol):
+    driver.get(current_url)
+    sleep(3)
+    print("i have awoken")
+    # csv_butt = driver.find_element_by_xpath("//*[text()='Get table as CSV (for Excel)']")
+    # csv_butt.click()
+    driver.execute_script("window.scrollTo(0, 400)") 
+    a = ActionChains(driver)
+    m = driver.find_element_by_xpath("//*[text()='Share & Export']")
+    a.move_to_element(m).perform()
+    # driver.implicitly_wait(1)
+    sleep(2)
+    n = driver.find_element_by_xpath("//*[text()='Get table as CSV (for Excel)']")
+    a.move_to_element(n).click().perform()
+
+    csv_stuff = driver.find_element_by_id("csv_" + lol + "_stats")
+    stats_list = str(csv_stuff.text).split('\n')
+
+    return stats_list[4:]
+
+# sl = getcsv(2021, "l")
+# f = open("text.csv", "w")
+# writer = csv.writer(f)
+# headers = sl.pop(0).split(',')
+# writer.writerow(headers)
+# print(headers)
+# for row in sl:
+#     writer.writerow(row.split(','))
+# f.close()
+
+stat = str(sys.argv[1])
+year = 1980
+while year <= 2021:
+    filepath = "./" + stat + "_stats/" + str(year) + stat + "_stats.csv"
+    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_" + stat + ".html"
+    stats_list = getcsv(url, stat)
+    with open(filepath, "w") as f:
+        writer = csv.writer(f)
+        headers = stats_list.pop(0).split(',')
+        writer.writerow(headers)
+
+        for row in stats_list:
+            writer.writerow(row.split(','))
+    year += 1
+
+driver.close()
